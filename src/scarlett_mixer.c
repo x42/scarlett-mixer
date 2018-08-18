7b031003f14696349f67b8f170bb569488269345
Robin Gareus <robin@gareus.org>
2018-08-18 14:38:50 +0200

    Update 6i6 mapping, match dump from #8
    https://github.com/x42/scarlett-mixer/issues/8#issuecomment-414020472

--- src/scarlett_mixer.c
+++ src/scarlett_mixer.c
@@ -106,15 +106,15 @@ static Device devices[] = {
 		.sin = 6, .sout = 6,
 		.smst = 3,
 		.num_hiz = 2,
-		.num_pad = 0,
-		.matrix_mix_offset = 33, .matrix_mix_stride = 9, // XXX stride should be 7, bug in kernel-driver ?!
-		.matrix_in_offset = 22, .matrix_in_stride = 9,   // XXX stride should be 7, bug in kernel-driver ?!
+		.num_pad = 4, // XXX does the device have pad? bug in kernel-driver?
+		.matrix_mix_offset = 25, .matrix_mix_stride = 9, // XXX stride should be 7, bug in kernel-driver ?!
+		.matrix_in_offset = 24, .matrix_in_stride = 9,   // XXX stride should be 7, bug in kernel-driver ?!
 		.out_gain_map = { 1 /* Monitor */, 4 /* Headphone */, 7 /* SPDIF */, -1, -1, -1 , -1, -1, -1, -1 },
 		.out_gain_labels = { "Monitor", "Headphone", "SPDIF", "", "", "", "", "", "", "" },
 		.out_bus_map = { 2, 3, 5, 6, 8, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
-		.input_offset = 16,
-		.hiz_map = { 10, 12 },
-		.pad_map = { -1, -1, -1, -1 },
+		.input_offset = 18,
+		.hiz_map = { 12, 14 },
+		.pad_map = { 13, 15, 16, 17 },
 	},
 	{
 		.name = "Scarlett 18i20 USB",
