42bc0826e314e667aa5e79401a8e4a3a526da934
Robin Gareus <robin@gareus.org>
2018-08-22 03:23:21 +0200

    Adjust 6i6 offsets to match @SpotlightKid's kernel/driver -- #8

--- src/scarlett_mixer.c
+++ src/scarlett_mixer.c
@@ -107,8 +107,8 @@ static Device devices[] = {
 		.smst = 3,
 		.num_hiz = 2,
 		.num_pad = 4, // XXX does the device have pad? bug in kernel-driver?
-		.matrix_mix_offset = 25, .matrix_mix_stride = 9, // XXX stride should be 7, bug in kernel-driver ?!
-		.matrix_in_offset = 24, .matrix_in_stride = 9,   // XXX stride should be 7, bug in kernel-driver ?!
+		.matrix_mix_offset = 26, .matrix_mix_stride = 9, // XXX stride should be 7, bug in kernel-driver ?!
+		.matrix_in_offset = 25, .matrix_in_stride = 9,   // XXX stride should be 7, bug in kernel-driver ?!
 		.out_gain_map = { 1 /* Monitor */, 4 /* Headphone */, 7 /* SPDIF */, -1, -1, -1 , -1, -1, -1, -1 },
 		.out_gain_labels = { "Monitor", "Headphone", "SPDIF", "", "", "", "", "", "", "" },
 		.out_bus_map = { 2, 3, 5, 6, 8, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
